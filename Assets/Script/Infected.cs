a23a1439ec24ea5d965cfcbd6e961514e4227f69
Johnny <blizzardkang@gmail.com>
2022-03-20 04:15:37 -0700

    Infected Pathways added

--- Assets/Script/Infected.cs
+++ Assets/Script/Infected.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Infected : MonoBehaviour
 {
+    public Transform Paths;
     // Start is called before the first frame update
-    void Start()
+
+
+    private void Start()
     {
+        Vector3[] waypoints = new Vector3[Paths.childCount];
+        for (int i = 0; i <waypoints.Length; i ++)
+        {
+            waypoints[i] = Paths.GetChild(i).position;
         
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDrawGizmos()
     {
-        
+        Vector3 startPosition = Paths.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform waypoint in Paths)
+        {
+            Gizmos.DrawSphere(waypoint.position, .3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
     }
 }
